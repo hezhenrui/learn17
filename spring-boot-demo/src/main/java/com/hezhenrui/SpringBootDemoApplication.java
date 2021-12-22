9be717af43fe824d1e238916601155207028e667
hezhenrui <zhenrui.he.code@outlook.com>
2021-12-22 16:04:35 +0800

    初始化项目-hezhenrui

---
+++ spring-boot-demo/src/main/java/com/hezhenrui/SpringBootDemoApplication.java
@@ -0,0 +1,35 @@
+package com.hezhenrui;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+    /**
+     * redisson配置类
+     */
+    @Configuration
+    public static class RedissonConfig {
+        @Bean
+        public RedissonClient redissonClient() {
+            Config config = new Config();
+            config.useClusterServers().setScanInterval(2000)
+                    .addNodeAddress("redis://127.0.0.1:6379", "redis://127.0.0.1:6380")
+                    .addNodeAddress("redis://127.0.0.1:6381", "redis://127.0.0.1:6382")
+                    .addNodeAddress("redis://127.0.0.1:6383", "redis://127.0.0.1:6384");
+            return Redisson.create(config);
+        }
+    }
+
+}
