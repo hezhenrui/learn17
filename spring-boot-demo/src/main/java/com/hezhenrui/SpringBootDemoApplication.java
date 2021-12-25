510afe058772f47e65fdfd719fc7be30422f483a
zhenrui.he <zhenrui.he..code@outlook.com>
2021-12-25 13:19:56 +0800

    注释redisson配置

--- spring-boot-demo/src/main/java/com/hezhenrui/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/hezhenrui/SpringBootDemoApplication.java
@@ -1,12 +1,12 @@
 package com.hezhenrui;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
+//import org.redisson.Redisson;
+//import org.redisson.api.RedissonClient;
+//import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
@@ -19,17 +19,17 @@ public class SpringBootDemoApplication {
     /**
      * redisson配置类
      */
-    @Configuration
-    public static class RedissonConfig {
-        @Bean
-        public RedissonClient redissonClient() {
-            Config config = new Config();
-            config.useClusterServers().setScanInterval(2000)
-                    .addNodeAddress("redis://127.0.0.1:6379", "redis://127.0.0.1:6380")
-                    .addNodeAddress("redis://127.0.0.1:6381", "redis://127.0.0.1:6382")
-                    .addNodeAddress("redis://127.0.0.1:6383", "redis://127.0.0.1:6384");
-            return Redisson.create(config);
-        }
-    }
+//    @Configuration
+//    public static class RedissonConfig {
+//        @Bean
+//        public RedissonClient redissonClient() {
+//            Config config = new Config();
+//            config.useClusterServers().setScanInterval(2000)
+//                    .addNodeAddress("redis://127.0.0.1:6379", "redis://127.0.0.1:6380")
+//                    .addNodeAddress("redis://127.0.0.1:6381", "redis://127.0.0.1:6382")
+//                    .addNodeAddress("redis://127.0.0.1:6383", "redis://127.0.0.1:6384");
+//            return Redisson.create(config);
+//        }
+//    }
 
 }
