faa826980484435f2ae84c276b69206f03f233b4
zhenrui.he <zhenrui.he..code@outlook.com>
2021-12-28 22:54:52 +0800

    ForkJoinPool示例

---
+++ spring-boot-demo/src/main/java/com/hezhenrui/service/Calculator.java
@@ -0,0 +1,13 @@
+package com.hezhenrui.service;
+
+public interface Calculator {
+
+    /**
+     * 把传进来的所有numbers 做求和处理
+     *
+     * @param numbers
+     * @return 总和
+     */
+    long sumUp(long[] numbers);
+}
+
