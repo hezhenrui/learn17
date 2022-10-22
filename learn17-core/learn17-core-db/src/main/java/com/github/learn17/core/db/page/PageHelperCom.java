790b3b1c5a5b164f3c783ddf5f95bad7f279e9e7
zhenrui.he <zhenrui.he.code@outlook.com>
2022-10-22 18:10:21 +0800

    配置修改

--- learn17-core/learn17-core-db/src/main/java/com/github/learn17/core/db/page/PageHelperCom.java
+++ learn17-core/learn17-core-db/src/main/java/com/github/learn17/core/db/page/PageHelperCom.java
@@ -20,8 +20,6 @@ public class PageHelperCom {
         //分页插件
         Interceptor interceptor = new PageInterceptor();
         Properties properties = new Properties();
-        //数据库
-        properties.setProperty("helperDialect", "mysql");
         //是否将参数offset作为PageNum使用
         properties.setProperty("offsetAsPageNum", "true");
         //是否进行count查询
@@ -30,6 +28,8 @@ public class PageHelperCom {
         // 注意的是reasonable参数，表示分页合理化，默认值为false。
         //如果该参数设置为 true 时，pageNum<=0 时会查询第一页，pageNum>pages（超过总数时），会查询最后一页。默认false 时，直接根据参数进行查询。
         properties.setProperty("reasonable", "false");
+        properties.setProperty("autoDialect","true");
+        properties.setProperty("autoRuntimeDialect","true");
         interceptor.setProperties(properties);
         sqlSessionFactory.setPlugins(interceptor);
     }
